1425de19339f1075fa0127ece6403b66ebab8281
Shashank <shashankpulluri1@gmail.com>
2025-10-07 18:13:30 +0530

    Removed Unnecessary imports

--- src/main/java/com/alpha/ABCLogistics/Entity/Unloading.java
+++ src/main/java/com/alpha/ABCLogistics/Entity/Unloading.java
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.SequenceGenerator;
 @Entity
 public class Unloading {
